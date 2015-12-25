e0bad7db37ebd3e12fca809ee3fe2e5906a54f36
Jerry Chang <therealjerrycan@gmail.com>
2015-12-25 14:45:09 -0800

    trying out the new es6 destructuring syntax

---
+++ src/destructuring.js
@@ -0,0 +1,54 @@
+// Array Matching 
+var [a, ,b] =  [1,3,4];
+
+// console.log(a);
+// console.log(b);
+
+ /* 
+    Fail-soft destructuring with defaults 
+        - Could assign default values for the variables in destructuring 
+        - 
+*/
+
+ var [c = 1, d = 2] = [5];
+ // console.log(c);
+ // console.log(d);
+
+
+
+// Object matching 
+function today(){
+    let dateToday = new Date();
+    return  {
+        d: dateToday.getDay(),
+        m: dateToday.getMonth(),
+        y: dateToday.getFullYear() 
+    }
+}
+
+var { m: month, y: year} = today();
+
+// console.log(month);
+// console.log(year);
+
+
+// Destructuring while passing params into functions 
+let person = {
+    firstName: "Joe",
+    lastName: "Blue",
+    age: 21,
+    gender: "M"
+}
+
+function getFullName({ firstName: fname, lastName: lname }) {
+    return `${fname} ${lname}`;
+}
+
+console.log(
+    getFullName({firstName: person.firstName, lastName: person.lastName })
+);
+
+
+
+
+
